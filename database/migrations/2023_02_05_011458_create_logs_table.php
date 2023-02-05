0b7bff8ad931a2ecfc03c2bf75767142e5201267
dimanda <traperwaze@gmail.com>
2023-02-05 08:26:33 +0700

    add log model and migration

---
+++ database/migrations/2023_02_05_011458_create_logs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('log_pool_id')->references('id')->on('log_pools');
+            $table->foreignId('student_id')->references('id')->on('students');
+            $table->boolean('available');
+            $table->boolean('bottle');
+            $table->boolean('warming');
+            $table->boolean('wearpack');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
