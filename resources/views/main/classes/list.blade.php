69885d21c17276ce294d6353d694b32a2db3499c
dimanda <traperwaze@gmail.com>
2023-02-08 20:27:52 +0700

    refine class list look

--- resources/views/main/classes/list.blade.php
+++ resources/views/main/classes/list.blade.php
@@ -6,23 +6,13 @@
             <p class="card-text fw-bold">Classes</p>
         </div>
         <div class="card-body">
-            <div class="table-responsive p-1">
-                <table class="table table-bordered">
-                    <thead>
-                    <tr>
-                        <th>Name</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($classes as $c)
-                        <tr>
-                            <td>
-                                <a href="{{ route('classes.students', $c->id) }}">{{ $c->name }}</a>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+            <div class="list-group">
+                @foreach($classes as $c)
+                    <a class="list-group-item list-group-item-action"
+                       href="{{ route('classes.students', $c->id) }}">
+                        {{ $c->name }}
+                    </a>
+                @endforeach
             </div>
         </div>
     </div>
